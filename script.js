67657e39b26d9de613ea8b201364cbcec389dded
mayararosarodrigues-oss <mayara.rosa.rodrigues@escola.pr.gov.br>
2026-09-22 13:41:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -172,3 +172,15 @@ export function aleatorio (lista){
     return lista[posicao];
     import {aleatorio} from ‘./aleatorio.js’;
 import {perguntas} from ‘./perguntas.js;
+const botaoJogarNovamente = document.querySelector(“.novamente-btn”);
+    function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    mostraPergunta();
+        function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+botaoJogarNovamente.addEventListener("click", jogaNovamente());
+
+}
