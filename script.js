9f977f1a62a7828ddae2b3ef04b4de0d809a5792
mayararosarodrigues-oss <mayara.rosa.rodrigues@escola.pr.gov.br>
2026-09-22 13:30:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -161,4 +161,9 @@ function respostaSelecionada(opcaoSelecionada){
         atual++;
         mostraPergunta();
 }
+function dadoD6() {
+    var numero = Math.floor(Math.random() * 6) + 1;
+    return numero;
+}
 
+console.log(dadoD6());
